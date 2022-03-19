9350be9ede8fe6f053df940a50d7550ada75c6f2
eskim_o <eunddo2001@naver.com>
2022-03-19 18:13:09 +0900

    2

--- 2주차/x86_handray.c
+++ 2주차/x86_handray.c
@@ -2,5 +2,15 @@
 
 int main()
 {
-    fgets
+    int a;
+    fgets(str, 45, stdin);
+    printf("\n[buf]: %s\n", str);
+    printf("[check] %p\n", 0x4030201);
+    
+
+
+    puts("\nYou are on the right way!");
+    puts("Yeah dude! You win!\nOpening your shell...");
+    system
+    puts("Shell closed! Bye.");
 }
