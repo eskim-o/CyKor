e5d08ebe05786604b2061133037aa99d1a09da55
eskim_o <eunddo2001@naver.com>
2022-03-20 04:28:17 +0900

    final

--- 2주차/x86_handray.c
+++ 2주차/x86_handray.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     int a = 0x4030201;
-    int str;
-    fgets(str, 45, stdin);
-    printf("\n[buf]: %s\n", str);
+    char str;
+
+    fgets(&str, 45, stdin);
+    printf("\n[buf]: %s\n", &str);
     printf("[check] %p\n", a);
-    if (a!=0x4030201)
+    if (a!=0x4030201 && a!=0xdeadbeef)
     {
-        if (a!=0xdeadbeef)
-        {
-            puts("\nYou are on the right way!");
-        }
+        puts("\nYou are on the right way!");
     }
+
     if (a==0xdeadbeef)
     {
         puts("Yeah dude! You win!\nOpening your shell...");
