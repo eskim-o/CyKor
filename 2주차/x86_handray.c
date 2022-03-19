3f9fc61abbd68296aea7e068e97b411eacb2dbfc
eskim_o <eunddo2001@naver.com>
2022-03-19 21:08:24 +0900

    3

--- 2주차/x86_handray.c
+++ 2주차/x86_handray.c
@@ -2,15 +2,24 @@
 
 int main()
 {
-    int a;
+    int a = 0x4030201;
+    int str;
     fgets(str, 45, stdin);
     printf("\n[buf]: %s\n", str);
-    printf("[check] %p\n", 0x4030201);
+    printf("[check] %p\n", a);
+    if (a!=0x4030201)
+    {
+        if (a!=0xdeadbeef)
+        {
+            puts("\nYou are on the right way!");
+        }
+    }
+    if (a==0xdeadbeef)
+    {
+        puts("Yeah dude! You win!\nOpening your shell...");
+        system("/bin/dash");
+        puts("Shell closed! Bye.");
+    }
     
-
-
-    puts("\nYou are on the right way!");
-    puts("Yeah dude! You win!\nOpening your shell...");
-    system
-    puts("Shell closed! Bye.");
+    return 0;
 }
