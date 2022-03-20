476afd43a57a0e95fcf568b7dc4d70513b0b0e5d
eskim_o <eunddo2001@naver.com>
2022-03-20 17:21:03 +0900

    final

--- 2주차/x64_handray.c
+++ 2주차/x64_handray.c
@@ -1,1 +1,32 @@
 #include <stdio.h>
+
+int main()
+{
+    setup();
+    while (1)
+    {
+        int a = 0;
+        int b = 0;
+        int c = 0;
+
+        int sum[11];
+
+        for (int i = 10; i > 0; i--)
+        {
+            sum[i] = 0;
+        }
+
+        printf("Input: ");
+
+        if (scanf("%ld %ld %ld", &a, &b, &c) == 3)
+        {
+            b = b + a;
+            sum[c] = b;
+            printf("Result: %ld", sum[7]);
+        }
+        else
+        {
+            break;
+        }
+    }
+}
